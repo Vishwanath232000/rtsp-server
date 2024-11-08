fc3c9439c9f6605ce0e8ece070288cee4fad8ab8
Vishwanath vishu <vishwanath20023@gmail.com>
2024-11-07 16:45:28 -0800

    Update getInstanceID function and aws client

--- internal/core/rtsp_session.go
+++ internal/core/rtsp_session.go
@@ -2,79 +2,79 @@ package core
 
 import (
 	"context"
-    // "encoding/hex"
-    "errors"
-    "fmt"
-    "net"
-    "sync"
-    "time"
-    // "io"
-    // "net/http"
-	"os"
+	// "encoding/hex"
+	"errors"
+	"fmt"
+	"net"
+	"sync"
+	"time"
+
+	// "io"
+	// "net/http"
 	"log"
-    "github.com/aler9/gortsplib"
-    "github.com/aler9/gortsplib/pkg/base"
-    "github.com/google/uuid"
-    "github.com/pion/rtp"
+	"os"
 
-    "github.com/bhaney/rtsp-simple-server/internal/conf"
-    "github.com/bhaney/rtsp-simple-server/internal/externalcmd"
-    "github.com/bhaney/rtsp-simple-server/internal/logger"
+	"github.com/aler9/gortsplib"
+	"github.com/aler9/gortsplib/pkg/base"
+	"github.com/google/uuid"
+	"github.com/pion/rtp"
 
-    // AWS SDK v2 imports
-    "github.com/aws/aws-sdk-go-v2/aws"
-    "github.com/aws/aws-sdk-go-v2/config"
-    "github.com/aws/aws-sdk-go-v2/service/dynamodb"
-    "github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
-    "github.com/aws/aws-sdk-go-v2/feature/ec2/imds"
+	"github.com/bhaney/rtsp-simple-server/internal/conf"
+	"github.com/bhaney/rtsp-simple-server/internal/externalcmd"
+	"github.com/bhaney/rtsp-simple-server/internal/logger"
+
+	// AWS SDK v2 imports
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/feature/ec2/imds"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 )
 
 // Global variables
 var (
-    dbSvc *dynamodb.Client
-    activeSessionCount int
-    countMutex sync.Mutex
-	dynamoDBTableName string
+	dbSvc              *dynamodb.Client
+	activeSessionCount int
+	countMutex         sync.Mutex
+	dynamoDBTableName  string
 )
 
-
-
 func init() {
-    // Load the AWS configuration from the environment, credentials file, or IAM role
-    cfg, err := config.LoadDefaultConfig(context.TODO(),
-        config.WithRegion("us-east-1"), // Replace with your desired region
-    )
-    if err != nil {
-        panic("unable to load SDK config, " + err.Error())
-    }
+	// Load the AWS configuration from the environment, credentials file, or IAM role
+	cfg, err := config.LoadDefaultConfig(context.TODO(),
+		config.WithRegion("us-east-1"), // Replace with your desired region
+	)
+	if err != nil {
+		panic("unable to load SDK config, " + err.Error())
+	}
 
-	
-    // Initialize DynamoDB client with the configuration
-    dbSvc = dynamodb.NewFromConfig(cfg)
+	// Initialize DynamoDB client with the configuration
+	dbSvc = dynamodb.NewFromConfig(cfg)
 
 	dynamoDBTableName = os.Getenv("DYNAMODB_TABLE_NAME")
-    if dynamoDBTableName == "" {
-        log.Fatal("DYNAMODB_TABLE_NAME environment variable is not set")
-		dynamoDBTableName="sam-rtsp-virtual-streams"
+	if dynamoDBTableName == "" {
+		log.Fatal("DYNAMODB_TABLE_NAME environment variable is not set")
+		dynamoDBTableName = "sam-rtsp-virtual-streams"
 
-    }
+	}
 }
 
 func getInstanceID() string {
-    ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
-    defer cancel()
-    
-    client := imds.New(imds.Options{})
-    
-    // Get instance ID from IMDS
-    instanceID,err := client.GetInstanceIdentityDocument(ctx, &imds.GetInstanceIdentityDocumentInput{})
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
 
-    if err != nil {
-        return "local-instance" // fallback for local testing
-    }
-    
-    return instanceID.InstanceID
+	client := imds.New(imds.Options{})
+
+	// Get instance ID from IMDS
+	instanceID, err := client.GetInstanceIdentityDocument(ctx, &imds.GetInstanceIdentityDocumentInput{})
+
+	if err != nil {
+		return "local-instance" // fallback for local testing
+	}
+
+	return instanceID.InstanceID
 }
+
 const (
 	pauseAfterAuthError = 2 * time.Second
 )
@@ -126,32 +126,30 @@ func newRTSPSession(
 		uuid:            uuid.New(),
 		created:         time.Now(),
 	}
-	s.log(logger.Debug,"newRTSPSession: Begin")
+	s.log(logger.Debug, "newRTSPSession: Begin")
 
-	
 	s.log(logger.Info, "created by %v", s.author.NetConn().RemoteAddr())
-	s.log(logger.Debug,"newRTSPSession: End-99")
-
+	s.log(logger.Debug, "newRTSPSession: End-99")
 
 	return s
 }
 
 // Close closes a Session.
 func (s *rtspSession) close() {
-	s.log(logger.Debug,"close: Begin")
+	s.log(logger.Debug, "close: Begin")
 	s.session.Close()
-	s.log(logger.Debug,"close: End-99")
-	
+	s.log(logger.Debug, "close: End-99")
+
 }
 
 // isRTSPSession implements pathRTSPSession.
 func (s *rtspSession) isRTSPSession() {}
 
 func (s *rtspSession) safeState() gortsplib.ServerSessionState {
-	s.log(logger.Debug,"safeState: Begin")
+	s.log(logger.Debug, "safeState: Begin")
 	s.stateMutex.Lock()
 	defer s.stateMutex.Unlock()
-	s.log(logger.Debug,"safeState: End-99")
+	s.log(logger.Debug, "safeState: End-99")
 	return s.state
 }
 
@@ -165,9 +163,8 @@ func (s *rtspSession) log(level logger.Level, format string, args ...interface{}
 }
 
 // onClose is called by rtspServer.
-// 
 func (s *rtspSession) onClose(err error) {
-	s.log(logger.Debug,"onClose: Begin")
+	s.log(logger.Debug, "onClose: Begin")
 	if s.session.State() == gortsplib.ServerSessionStatePlay {
 		if s.onReadCmd != nil {
 			s.onReadCmd.Close()
@@ -175,14 +172,13 @@ func (s *rtspSession) onClose(err error) {
 			s.log(logger.Info, "runOnRead command stopped")
 		}
 	}
-	
 
 	switch s.session.State() {
 	case gortsplib.ServerSessionStatePrePlay, gortsplib.ServerSessionStatePlay:
 		s.path.readerRemove(pathReaderRemoveReq{author: s})
 
 	case gortsplib.ServerSessionStatePreRecord, gortsplib.ServerSessionStateRecord:
-		
+
 		s.path.publisherRemove(pathPublisherRemoveReq{author: s})
 		countMutex.Lock()
 		activeSessionCount--
@@ -190,12 +186,11 @@ func (s *rtspSession) onClose(err error) {
 		countMutex.Unlock()
 		timestamp := time.Now().UTC().Format(time.RFC3339)
 
-		fmt.Printf(time_stamp," | %s | STOPPED | %s | %s\n", formattedSessionCount, s.uuid, s.path.Name())
-		s.log(logger.Info,"| %s | STOPPED | %s | %s\n", formattedSessionCount, s.uuid, s.path.Name())
-		
+		fmt.Printf(timestamp, " | %s | STOPPED | %s | %s\n", formattedSessionCount, s.uuid, s.path.Name())
+		s.log(logger.Info, "| %s | STOPPED | %s | %s\n", formattedSessionCount, s.uuid, s.path.Name())
+
 		// Only log to DynamoDB and print stop message for publishers
-		
-		
+
 		input := &dynamodb.UpdateItemInput{
 			TableName: aws.String(dynamoDBTableName),
 			Key: map[string]types.AttributeValue{
@@ -203,7 +198,7 @@ func (s *rtspSession) onClose(err error) {
 					Value: s.path.Name(),
 				},
 			},
-			UpdateExpression: aws.String("SET time_stamp = :time_stamp, is_active = :is_active"),
+			UpdateExpression:    aws.String("SET time_stamp = :time_stamp, is_active = :is_active"),
 			ConditionExpression: aws.String("is_active = :is_active_condition"),
 			ExpressionAttributeValues: map[string]types.AttributeValue{
 				":time_stamp": &types.AttributeValueMemberS{
@@ -218,16 +213,15 @@ func (s *rtspSession) onClose(err error) {
 			},
 		}
 
-
 		// Update DynamoDB asynchronously
 		go func() {
-			_, err := dbSvc.UpdateItem(context.TODO(), input)  // Passing context as required
+			_, err := dbSvc.UpdateItem(context.TODO(), input) // Passing context as required
 			if err != nil {
 				s.log(logger.Error, "failed to log stream stop to DynamoDB: %v", err)
 			}
 		}()
 		// rtsp_path := s.path.Name()
-		
+
 		// fmt.Println("|", activeSessionCount ,"|", rtsp_path ,"|", s.uuid ,"| ( Stopped )")
 		// countMutex.Lock()
 		// activeSessionCount--
@@ -236,10 +230,8 @@ func (s *rtspSession) onClose(err error) {
 
 		// fmt.Printf("| %s | STOPPED | %s | %s\n", formattedSessionCount, s.uuid, s.path.Name())
 
-		
-		
 	}
-	s.log(logger.Debug,"onClose: End-99")
+	s.log(logger.Debug, "onClose: End-99")
 
 	s.path = nil
 	s.stream = nil
@@ -247,7 +239,7 @@ func (s *rtspSession) onClose(err error) {
 
 // onAnnounce is called by rtspServer.
 func (s *rtspSession) onAnnounce(c *rtspConn, ctx *gortsplib.ServerHandlerOnAnnounceCtx) (*base.Response, error) {
-	s.log(logger.Debug,"onAnnounce: Begin")
+	s.log(logger.Debug, "onAnnounce: Begin")
 	res := s.pathManager.publisherAdd(pathPublisherAddReq{
 		author:   s,
 		pathName: ctx.Path,
@@ -284,7 +276,7 @@ func (s *rtspSession) onAnnounce(c *rtspConn, ctx *gortsplib.ServerHandlerOnAnno
 	s.stateMutex.Lock()
 	s.state = gortsplib.ServerSessionStatePreRecord
 	s.stateMutex.Unlock()
-	s.log(logger.Debug,"onAnnounce: End-99")
+	s.log(logger.Debug, "onAnnounce: End-99")
 
 	return &base.Response{
 		StatusCode: base.StatusOK,
@@ -294,14 +286,14 @@ func (s *rtspSession) onAnnounce(c *rtspConn, ctx *gortsplib.ServerHandlerOnAnno
 // onSetup is called by rtspServer.
 func (s *rtspSession) onSetup(c *rtspConn, ctx *gortsplib.ServerHandlerOnSetupCtx,
 ) (*base.Response, *gortsplib.ServerStream, error) {
-	s.log(logger.Debug,"onSetup: Begin")
+	s.log(logger.Debug, "onSetup: Begin")
 	// in case the client is setupping a stream with UDP or UDP-multicast, and these
 	// transport protocols are disabled, gortsplib already blocks the request.
 	// we have only to handle the case in which the transport protocol is TCP
 	// and it is disabled.
 	if ctx.Transport == gortsplib.TransportTCP {
 		if _, ok := s.protocols[conf.Protocol(gortsplib.TransportTCP)]; !ok {
-			s.log(logger.Debug,"onSetup: End-1")
+			s.log(logger.Debug, "onSetup: End-1")
 			return &base.Response{
 				StatusCode: base.StatusUnsupportedTransport,
 			}, nil, nil
@@ -318,7 +310,7 @@ func (s *rtspSession) onSetup(c *rtspConn, ctx *gortsplib.ServerHandlerOnSetupCt
 				pathUser conf.Credential,
 				pathPass conf.Credential,
 			) error {
-				s.log(logger.Debug,"onSetup: End-1")
+				s.log(logger.Debug, "onSetup: End-1")
 				return c.authenticate(ctx.Path, pathIPs, pathUser, pathPass, false, ctx.Request, ctx.Query)
 			},
 		})
@@ -327,24 +319,24 @@ func (s *rtspSession) onSetup(c *rtspConn, ctx *gortsplib.ServerHandlerOnSetupCt
 			switch terr := res.err.(type) {
 			case pathErrAuthNotCritical:
 				s.log(logger.Debug, "non-critical authentication error: %s", terr.message)
-				s.log(logger.Debug,"onSetup: End-2")
+				s.log(logger.Debug, "onSetup: End-2")
 				return terr.response, nil, nil
 
 			case pathErrAuthCritical:
 				// wait some seconds to stop brute force attacks
 				<-time.After(pauseAfterAuthError)
 
-				s.log(logger.Debug,"onSetup: End-3")
+				s.log(logger.Debug, "onSetup: End-3")
 				return terr.response, nil, errors.New(terr.message)
 
 			case pathErrNoOnePublishing:
-				s.log(logger.Debug,"onSetup: End-4")
+				s.log(logger.Debug, "onSetup: End-4")
 				return &base.Response{
 					StatusCode: base.StatusNotFound,
 				}, nil, res.err
 
 			default:
-				s.log(logger.Debug,"onSetup: End-5")
+				s.log(logger.Debug, "onSetup: End-5")
 				return &base.Response{
 					StatusCode: base.StatusBadRequest,
 				}, nil, res.err
@@ -355,7 +347,7 @@ func (s *rtspSession) onSetup(c *rtspConn, ctx *gortsplib.ServerHandlerOnSetupCt
 		s.stream = res.stream
 
 		if ctx.TrackID >= len(res.stream.tracks()) {
-			s.log(logger.Debug,"onSetup: End-6")
+			s.log(logger.Debug, "onSetup: End-6")
 			return &base.Response{
 				StatusCode: base.StatusBadRequest,
 			}, nil, fmt.Errorf("track %d does not exist", ctx.TrackID)
@@ -365,13 +357,13 @@ func (s *rtspSession) onSetup(c *rtspConn, ctx *gortsplib.ServerHandlerOnSetupCt
 		s.state = gortsplib.ServerSessionStatePrePlay
 		s.stateMutex.Unlock()
 
-		s.log(logger.Debug,"onSetup: End-7")
+		s.log(logger.Debug, "onSetup: End-7")
 		return &base.Response{
 			StatusCode: base.StatusOK,
 		}, res.stream.rtspStream, nil
 
 	default: // record
-		s.log(logger.Debug,"onSetup: End-99")
+		s.log(logger.Debug, "onSetup: End-99")
 		return &base.Response{
 			StatusCode: base.StatusOK,
 		}, nil, nil
@@ -380,7 +372,7 @@ func (s *rtspSession) onSetup(c *rtspConn, ctx *gortsplib.ServerHandlerOnSetupCt
 
 // onPlay is called by rtspServer.
 func (s *rtspSession) onPlay(ctx *gortsplib.ServerHandlerOnPlayCtx) (*base.Response, error) {
-	s.log(logger.Debug,"onPlay: Begin")
+	s.log(logger.Debug, "onPlay: Begin")
 	h := make(base.Header)
 
 	if s.session.State() == gortsplib.ServerSessionStatePrePlay {
@@ -393,7 +385,6 @@ func (s *rtspSession) onPlay(ctx *gortsplib.ServerHandlerOnPlayCtx) (*base.Respo
 			n++
 		}
 
-		
 		// Only log basic info for readers, no special formatting
 		s.log(logger.Info, "is reading from path '%s', with %s, %s",
 			s.path.Name(),
@@ -416,7 +407,7 @@ func (s *rtspSession) onPlay(ctx *gortsplib.ServerHandlerOnPlayCtx) (*base.Respo
 		s.state = gortsplib.ServerSessionStatePlay
 		s.stateMutex.Unlock()
 	}
-	s.log(logger.Debug,"onPlay: End-99")
+	s.log(logger.Debug, "onPlay: End-99")
 
 	return &base.Response{
 		StatusCode: base.StatusOK,
@@ -425,36 +416,35 @@ func (s *rtspSession) onPlay(ctx *gortsplib.ServerHandlerOnPlayCtx) (*base.Respo
 }
 
 func (s *rtspSession) onRecord(ctx *gortsplib.ServerHandlerOnRecordCtx) (*base.Response, error) {
-	s.log(logger.Debug,"onRecord: Begin")
+	s.log(logger.Debug, "onRecord: Begin")
 	res := s.path.publisherStart(pathPublisherStartReq{
 		author:             s,
 		tracks:             s.session.AnnouncedTracks(),
 		generateRTPPackets: false,
 	})
 	if res.err != nil {
-		s.log(logger.Debug,"onRecord: End-1")
+		s.log(logger.Debug, "onRecord: End-1")
 		return &base.Response{
 			StatusCode: base.StatusBadRequest,
 		}, res.err
 	}
 	countMutex.Lock()
-	
+
 	activeSessionCount++
 	formattedSessionCount := fmt.Sprintf("%06d", activeSessionCount) // Pads to 6 digits with leading zeros
 	countMutex.Unlock()
 	// s.log(logger.Debug,"onRecord: End-2")
 	timestamp := time.Now().UTC().Format(time.RFC3339)
 
-	fmt.Printf(time_stamp," | %s | STARTED | %s | %s\n", formattedSessionCount, s.uuid, s.path.Name())
+	fmt.Printf(timestamp, " | %s | STARTED | %s | %s\n", formattedSessionCount, s.uuid, s.path.Name())
 
-	s.log(logger.Info,"| %s | STARTED | %s | %s\n", formattedSessionCount, s.uuid, s.path.Name())
-
+	s.log(logger.Info, "| %s | STARTED | %s | %s\n", formattedSessionCount, s.uuid, s.path.Name())
 
 	// Log publisher start
 	// fmt.Println("[",s.path.Name(),"]",":", s.uuid, ">>> Started")
 
 	// Log to DynamoDB for publishers
-	
+
 	input := &dynamodb.PutItemInput{
 		TableName: aws.String(dynamoDBTableName),
 		Item: map[string]types.AttributeValue{
@@ -480,7 +470,7 @@ func (s *rtspSession) onRecord(ctx *gortsplib.ServerHandlerOnRecordCtx) (*base.R
 	}
 
 	go func() {
-		_, err := dbSvc.PutItem(context.TODO(), input)  // Passing context as required
+		_, err := dbSvc.PutItem(context.TODO(), input) // Passing context as required
 		if err != nil {
 			s.log(logger.Error, "failed to log stream start to DynamoDB: %v", err)
 		}
@@ -498,8 +488,7 @@ func (s *rtspSession) onRecord(ctx *gortsplib.ServerHandlerOnRecordCtx) (*base.R
 
 	// fmt.Printf("| %s | STARTED | %s | %s\n", formattedSessionCount, s.uuid, s.path.Name())
 
-	s.log(logger.Debug,"onRecord: End-99")
-	
+	s.log(logger.Debug, "onRecord: End-99")
 
 	return &base.Response{
 		StatusCode: base.StatusOK,
@@ -508,7 +497,7 @@ func (s *rtspSession) onRecord(ctx *gortsplib.ServerHandlerOnRecordCtx) (*base.R
 
 // onPause is called by rtspServer.
 func (s *rtspSession) onPause(ctx *gortsplib.ServerHandlerOnPauseCtx) (*base.Response, error) {
-	s.log(logger.Debug,"onPause: Begin")
+	s.log(logger.Debug, "onPause: Begin")
 	switch s.session.State() {
 	case gortsplib.ServerSessionStatePlay:
 		if s.onReadCmd != nil {
@@ -530,7 +519,7 @@ func (s *rtspSession) onPause(ctx *gortsplib.ServerHandlerOnPauseCtx) (*base.Res
 		s.stateMutex.Unlock()
 	}
 
-	s.log(logger.Debug,"onPause: End-99")
+	s.log(logger.Debug, "onPause: End-99")
 
 	return &base.Response{
 		StatusCode: base.StatusOK,
@@ -544,7 +533,7 @@ func (s *rtspSession) onReaderData(data data) {
 
 // apiReaderDescribe implements reader.
 func (s *rtspSession) apiReaderDescribe() interface{} {
-	s.log(logger.Debug,"apiReaderDescribe: Begin")
+	s.log(logger.Debug, "apiReaderDescribe: Begin")
 	var typ string
 	if s.isTLS {
 		typ = "rtspsSession"
@@ -552,7 +541,7 @@ func (s *rtspSession) apiReaderDescribe() interface{} {
 		typ = "rtspSession"
 	}
 
-	s.log(logger.Debug,"apiReaderDescribe: End-99")
+	s.log(logger.Debug, "apiReaderDescribe: End-99")
 	return struct {
 		Type string `json:"type"`
 		ID   string `json:"id"`
@@ -561,14 +550,14 @@ func (s *rtspSession) apiReaderDescribe() interface{} {
 
 // apiSourceDescribe implements source.
 func (s *rtspSession) apiSourceDescribe() interface{} {
-	s.log(logger.Debug,"apiSourceDescribe: Begin")
+	s.log(logger.Debug, "apiSourceDescribe: Begin")
 	var typ string
 	if s.isTLS {
 		typ = "rtspsSession"
 	} else {
 		typ = "rtspSession"
 	}
-	s.log(logger.Debug,"apiSourceDescribe: End-99")
+	s.log(logger.Debug, "apiSourceDescribe: End-99")
 
 	return struct {
 		Type string `json:"type"`
@@ -578,7 +567,7 @@ func (s *rtspSession) apiSourceDescribe() interface{} {
 
 // onPacketRTP is called by rtspServer.
 func (s *rtspSession) onPacketRTP(ctx *gortsplib.ServerHandlerOnPacketRTPCtx) {
-	s.log(logger.Debug,"rtsp_session.go> onPacketRTP: Begin")
+	s.log(logger.Debug, "rtsp_session.go> onPacketRTP: Begin")
 	var err error
 
 	switch s.session.AnnouncedTracks()[ctx.TrackID].(type) {
@@ -607,7 +596,7 @@ func (s *rtspSession) onPacketRTP(ctx *gortsplib.ServerHandlerOnPacketRTPCtx) {
 	if err != nil {
 		s.log(logger.Warn, "%v", err)
 	}
-	s.log(logger.Debug,"rtsp_session.go> onPacketRTP: End-99")
+	s.log(logger.Debug, "rtsp_session.go> onPacketRTP: End-99")
 }
 
 // onDecodeError is called by rtspServer.
